54d9c683eb97a53d2dec0b1e5d5947596587f1a0
Kelly Hutchison <kellyhutchison@hotmail.com>
2022-11-17 16:16:31 +0000

    update js file with post form function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,30 @@
-const API_KEY = "D-wJomfVXtlkIsK3BhjQ9JgfRx4";
+const API_KEY = "VZn-Fx2ohpPGHAs_RhP_m6IgxYA";
 const API_URL = "https://ci-jshint.herokuapp.com/api";
-const resultModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+document.getElementById("submit").addEventListener("click", e => postForm(e));
+
+async function postForm(e) {
+
+    const form = new FormData(document.getElementById("checksform"));
+
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Authorization": API_KEY,
+        },
+        body: form,
+    });
+
+}
 
 async function getStatus(e) {
+
     const queryString = `${API_URL}?api_key=${API_KEY}`;
+
     const response = await fetch(queryString);
+
     const data = await response.json();
 
     if (response.ok) {
@@ -14,15 +32,17 @@ async function getStatus(e) {
     } else {
         throw new Error(data.error);
     }
+
 }
 
 function displayStatus(data) {
+
     let heading = "API Key Status";
     let results = `<div>Your key is valid until</div>`;
     results += `<div class="key-status">${data.expiry}</div>`;
 
     document.getElementById("resultsModalTitle").innerText = heading;
     document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
 
-    resultsModal.show()
 }
